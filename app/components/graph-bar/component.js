483b7ccbe94262a5d7de6269bd7a221cc184061d
Michal Bryxí <michal.bryxi@gmail.com>
2015-05-29 12:21:14 +0200

    Highcharts

--- app/components/graph-bar/component.js
+++ app/components/graph-bar/component.js
@@ -1,7 +1,146 @@
 import Ember from 'ember';
+// import defaultTheme from '../themes/default-theme';
 
 export default Ember.Component.extend({
   tagName: '',
+
+  chartMode: 'StockChart', // Available options: a falsy value, 'StockChart', 'Map'.
+                           // If `mode` is not provided or is a falsy value, the chart is initialized in Charts mode.
+                           // If `mode` is a string, it is passed to Highcharts as the first argument.
+                           // When Highcharts introduces a new mode, you will be able to use it here right away.
+
+  chartOptions: {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+          text: 'Fruit eaten'
+      }
+    }
+  },
+
+  chartData: [
+    {
+      name: 'Jane',
+      data: [1, 0, 4]
+    }, {
+      name: 'John',
+      data: [5, 7, 3]
+    }
+  ],
+
+  theme: {
+      colors: [
+        '#258be2',
+        '#666666',
+        '#f45b5b',
+        '#8085e9',
+        '#8d4654',
+        '#7798bf',
+        '#aaeeee',
+        '#ff0066',
+        '#eeaaee',
+        '#55bf3b',
+        '#df5353',
+        '#7798bf',
+        '#aaeeee'
+      ],
+      chart: {
+        backgroundColor: null,
+        style: {
+          fontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+        }
+      },
+      title: {
+        style: {
+          color: 'black',
+          fontSize: '18px',
+          fontWeight: 'bold'
+        }
+      },
+      subtitle: {
+        style: {
+          color: 'black'
+        }
+      },
+      tooltip: {
+        borderWidth: 0,
+        style: {
+          fontSize: '16px'
+        }
+      },
+      legend: {
+        itemStyle: {
+          fontWeight: 'bold',
+          fontSize: '14px'
+        }
+      },
+      xAxis: {
+        labels: {
+          style: {
+            color: '#6e6e70',
+            fontSize: '16px'
+          }
+        },
+        title: {
+          style: {
+            fontSize: '14px'
+          }
+        }
+      },
+      yAxis: {
+        labels: {
+          style: {
+            color: '#6e6e70',
+            fontSize: '16px'
+          }
+        },
+        title: {
+          style: {
+            fontSize: '14px'
+          }
+        }
+      },
+      plotOptions: {
+        series: {
+          shadow: true
+        },
+        candlestick: {
+          lineColor: '#404048'
+        }
+      },
+      navigator: {
+        xAxis: {
+          gridLineColor: '#D0D0D8'
+        }
+      },
+      rangeSelector: {
+        buttonTheme: {
+          fill: 'white',
+          stroke: '#C0C0C8',
+          'stroke-width': 1,
+          states: {
+            select: {
+              fill: '#D0D0D8'
+            }
+          }
+        }
+      },
+      scrollbar: {
+        trackBorderColor: '#C0C0C8'
+      },
+      background2: '#E0E0E8',
+      global: {
+        timezoneOffset: new Date().getTimezoneOffset()
+      }
+    },
 
   handoverNames: function () {
     return this.get('filteredHandovers').map(function (item) {
